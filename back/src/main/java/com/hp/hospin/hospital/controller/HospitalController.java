dd9638d106777dc91a066c65e1c64c3cbf3de884
jae9380 <ljy5314@gmail.com>
2025-08-19 17:21:46 +0900

    feat: Specification 기반 검색/정렬/페이징 구현

--- back/src/main/java/com/hp/hospin/hospital/controller/HospitalController.java
+++ back/src/main/java/com/hp/hospin/hospital/controller/HospitalController.java
@@ -2,8 +2,11 @@ package com.hp.hospin.hospital.controller;
 
 import com.hp.hospin.hospital.dto.HospitalInfoResponse;
 import com.hp.hospin.hospital.dto.HospitalListResponse;
+import com.hp.hospin.hospital.dto.HospitalSearchRequest;
 import com.hp.hospin.hospital.service.HospitalService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -28,5 +31,21 @@ public class HospitalController {
     public List<HospitalListResponse> getNearbyHospitals(@RequestParam String latitude,
                                                          @RequestParam String longitude) {
         return hospitalService.getHospitalsNearby(latitude, longitude);
+    }
+
+    @GetMapping("/search")
+    public Page<HospitalListResponse> search(
+            @RequestParam(required = false) String name,
+            @RequestParam(required = false) Long categoryCode,
+            @RequestParam(required = false) Long regionCode,
+            @RequestParam(required = false) Long districtCode,
+            @RequestParam(required = false) Long postalCode,
+            @RequestParam(required = false) String address,
+            Pageable pageable
+    ) {
+        HospitalSearchRequest req = new HospitalSearchRequest(
+                name, categoryCode, regionCode, districtCode, postalCode, address
+        );
+        return hospitalService.search(req, pageable);
     }
 }
--- back/src/main/java/com/hp/hospin/hospital/repository/HospitalRepository.java
+++ back/src/main/java/com/hp/hospin/hospital/repository/HospitalRepository.java
@@ -2,6 +2,7 @@ package com.hp.hospin.hospital.repository;
 
 import com.hp.hospin.hospital.entity.Hospital;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -10,7 +11,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface HospitalRepository extends JpaRepository<Hospital, String> {
+public interface HospitalRepository extends JpaRepository<Hospital, String>, JpaSpecificationExecutor<Hospital> {
     Optional<Hospital> findByHospitalCode(String hospitalCode);
 
     @Query(value = "SELECT * FROM hospitals h " +
