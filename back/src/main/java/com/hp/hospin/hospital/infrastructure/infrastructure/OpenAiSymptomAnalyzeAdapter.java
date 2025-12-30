6dc2d3d4f5cb30ea2db82ee56c8340a2d7b06d0f
jae9380 <ljy5314@gmail.com>
2025-12-30 16:33:45 +0900

    feat: AI client Configuration

--- back/src/main/java/com/hp/hospin/hospital/infrastructure/infrastructure/OpenAiSymptomAnalyzeAdapter.java
+++ back/src/main/java/com/hp/hospin/hospital/infrastructure/infrastructure/OpenAiSymptomAnalyzeAdapter.java
@@ -2,12 +2,7 @@ package com.hp.hospin.hospital.infrastructure.infrastructure;
 
 import com.hp.hospin.hospital.application.port.SymptomAnalyzePort;
 import lombok.RequiredArgsConstructor;
-import org.springframework.ai.chat.messages.SystemMessage;
-import org.springframework.ai.chat.messages.UserMessage;
-import org.springframework.ai.chat.model.ChatResponse;
-import org.springframework.ai.chat.prompt.Prompt;
-import org.springframework.ai.openai.OpenAiChatModel;
-import org.springframework.ai.openai.OpenAiChatOptions;
+import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -15,22 +10,13 @@ import java.util.List;
 @Component
 @RequiredArgsConstructor
 public class OpenAiSymptomAnalyzeAdapter implements SymptomAnalyzePort {
-    private final OpenAiChatModel openAiChatModel;
+    private final ChatClient symptomAnalyzeChatClient;
 
     @Override
     public String analyze(String text) {
-
-        SystemMessage systemMessage = new SystemMessage("");
-        UserMessage userMessage = new UserMessage(text);
-
-        OpenAiChatOptions options = OpenAiChatOptions.builder()
-                .model("gpt-5-nano")
-                .temperature(0.3)
-                .build();
-
-        Prompt prompt = new Prompt(List.of(systemMessage, userMessage), options);
-
-        ChatResponse response = openAiChatModel.call(prompt);
-        return response.getResult().getOutput().getText();
+        return symptomAnalyzeChatClient.prompt()
+                .user(text)
+                .call()
+                .content();
     }
 }
