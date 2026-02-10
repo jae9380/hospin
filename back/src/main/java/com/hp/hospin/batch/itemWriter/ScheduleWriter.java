97d2d67e785aa6bd69fe5e6169beccf2e4d4920f
jae9380 <ljy5314@gmail.com>
2026-02-10 21:24:44 +0900

    feat: batch job metrics and Grafana dashboard update

---
+++ back/src/main/java/com/hp/hospin/batch/itemWriter/ScheduleWriter.java
@@ -0,0 +1,31 @@
+package com.hp.hospin.batch.itemWriter;
+
+import com.hp.hospin.notification.domain.port.FCMSender;
+import com.hp.hospin.notification.persentation.port.NotificationService;
+import com.hp.hospin.schedule.domain.entity.Schedule;
+import com.hp.hospin.schedule.infrastructure.mapper.SchedulePersistenceMapper;
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.item.Chunk;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.stereotype.Component;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class ScheduleWriter implements ItemWriter<Schedule> {
+    private final SchedulePersistenceMapper mapper;
+    private final EntityManager entityManager;
+
+    @Override
+    public void write(Chunk<? extends Schedule> chunk) {
+        chunk.forEach(schedule -> entityManager.merge(mapper.domainToJpa(schedule)));
+
+    }
+
+}
