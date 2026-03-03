86b93b16bfa5e8b2b47188616bec9a2d3e425b33
jae9380 <ljy5314@gmail.com>
2026-03-03 17:18:20 +0900

    chore: mailSender 추가

---
+++ back/src/main/java/com/hp/hospin/member/domain/AuthDomainServiceImpl.java
@@ -0,0 +1,13 @@
+package com.hp.hospin.member.domain;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Objects;
+
+@Service
+@RequiredArgsConstructor
+public class AuthDomainServiceImpl {
+
+}
